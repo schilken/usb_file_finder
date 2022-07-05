8b7d928630e176b3a640e5c414fdf8a27b3fe4e2
Alfred Schilken <alfred@schilken.de>
2022-07-05 10:55:52 +0200

    add clear button to searchfields in toolbar

--- lib/toolbar_searchfield.dart
+++ lib/toolbar_searchfield.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+//import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 import 'package:macos_ui/macos_ui.dart';
 
 typedef StringCallback = void Function(String);
@@ -20,7 +20,7 @@ class ToolbarSearchfield extends ToolbarItem {
   Widget build(BuildContext context, ToolbarItemDisplayMode displayMode) {
     return SizedBox(
       width: 120,
-      child: HookedSearchField(
+      child: _WrappedSearchField(
         placeholder: placeholder,
         onChanged: onChanged,
         onSubmitted: onSubmitted,
@@ -29,8 +29,8 @@ class ToolbarSearchfield extends ToolbarItem {
   }
 }
 
-class HookedSearchField extends HookWidget {
-  const HookedSearchField({
+class _WrappedSearchField extends StatelessWidget {
+  const _WrappedSearchField({
     Key? key,
     this.placeholder,
     required this.onChanged,
@@ -44,14 +44,10 @@ class HookedSearchField extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    controller.addListener(() {
-      onChanged('');
-    });
     return MacosTextField(
-      controller: controller,
       placeholder: placeholder,
       maxLines: 1,
+      clearButtonMode: OverlayVisibilityMode.editing,
       onTap: () {},
       onChanged: onChanged,
       onSubmitted: onSubmitted,
